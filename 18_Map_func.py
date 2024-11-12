6651cfc5cb9a2d55ce97f0d69c3edf6d301bf5fa
B-Rohan-Rao <raorohan060@gmail.com>
2024-11-12 08:46:13 +0530

    map and reduce

--- 18_Map_func.py
+++ 18_Map_func.py
@@ -1,14 +1,20 @@
 # map function provide the functionality to the elements of any iterable items.
 
-def cube(x):
-    return x**3
-
 l= [1, 4, 7, 8, 5, 2, 3]
 # newl = []
 # for i in l:
 #     newl.append(cube(i))
 # print(newl)
-
-
-newl = list(map(cube, l))
+newl = list(map(lambda x: x*x*x, l))
 print(newl)
+
+
+
+# returns elements based on the the case if the qualify the test case or not
+
+def filter_function(a):
+    return a > 4
+
+l= [1, 4, 7, 8, 5, 2, 3]
+newl = list(filter(filter_function, l))
+print(newl) # prints [7, 8, 5]
